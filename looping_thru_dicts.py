a7c52759616c977d2181a48d51c831a2d975e791
Rob Welsh <robpwelsh@gmail.com>
2023-03-05 18:31:41 -0500

    Add looping_thru_dicts.py

---
+++ looping_thru_dicts.py
@@ -0,0 +1,11 @@
+guests = {'Tim': 22, 'Tonya': 45, 'Mary': 12, 'Ann': 32, 'Beth': 20, 'Sam': 5, 'Manny': 76, 'Kenton': 15, 'Kenny': 27, 'Dixie': 46, 'Jane': 35, 'Mallory': 32, 'Julian': 4, 'Edward': 71, 'Rose': 65}
+
+for i in guests:
+    print(i)  # Prints each name on a separate line
+
+for i in guests:
+    print(guests[i])  # Prints each age on a separate line
+
+for i in guests:
+    print(guests)  # Prints entire dict on a separate line
+
