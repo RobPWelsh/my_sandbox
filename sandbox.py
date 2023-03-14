fd804bdff528805ecf03ca81c64804f1ada1cba0
Rob Welsh <robpwelsh@gmail.com>
2023-03-14 06:59:32 -0400

    Update sandbox.py with namedtuple

--- sandbox.py
+++ sandbox.py
@@ -36,4 +36,28 @@ num_collision_a = Counter([move[2] for move in deque_moves if 'A' in move])[True
 
 print(num_collision_a)
 
+BotScoreData = namedtuple('name', 'num_moves', 'num_collisions', 'score')
+
+# # Named Tuple
+# clothes = [('t-shirt', 'green', 'large', 9.99),
+#            ('jeans', 'blue', 'medium', 14.99),
+#            ('jacket', 'black', 'x-large', 19.99),
+#            ('t-shirt', 'grey', 'small', 8.99),
+#            ('shoes', 'white', '12', 24.99),
+#            ('t-shirt', 'grey', 'small', 8.99)]
+#
+# ClothingItem = namedtuple('ClothingItem', ['type', 'color', 'size', 'price'])
+#
+# new_coat = ClothingItem('coat', 'black', 'small', 14.99)
+# print(new_coat)  # Prints ClothingItem(type='coat', color='black', size='small', price=14.99)
+#
+# coat_cost = new_coat.price
+# print(coat_cost)  # Prints 14.99
+#
+# updated_clothes_data = []
+#
+# for item in clothes:
+#     updated_clothes_data.append(ClothingItem(item[0], item[1], item[2], item[3]))
+#
+# print(updated_clothes_data)  # Prints [ClothingItem(type='t-shirt', color='green', size='large', price=9.99), ClothingItem(type='jeans', color='blue', size='medium', price=14.99), ClothingItem(type='jacket', color='black', size='x-large', price=19.99), ClothingItem(type='t-shirt', color='grey', size='small', price=8.99), ClothingItem(type='shoes', color='white', size='12', price=24.99), ClothingItem(type='t-shirt', color='grey', size='small', price=8.99)]
 
