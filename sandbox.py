b0d3adb86b3d2bb9b5c899cddda8637ec1cfa6cf
Rob Welsh <robpwelsh@gmail.com>
2023-04-17 21:42:27 -0400

    Add simple example of passing an object as a parameter.

--- sandbox.py
+++ sandbox.py
@@ -4,6 +4,7 @@ from dataclasses import asdict
 import os
 import pathlib
 
+
 @dataclass
 class Card:
     summary: str = None
@@ -20,26 +21,33 @@ class Card:
 
 
 my_dict = {'summary': 'do something', 'owner': 'Rob', 'state': 'todo', 'id': 1}
-
 card1 = Card.from_dict(my_dict)
+print(card1)  # Card(summary='do something', owner='Rob', state='todo', id=1)
+print(card1.to_dict())  # {'summary': 'do something', 'owner': 'Rob', 'state': 'todo', 'id': 1}
 
-print(card1)
+card2 = Card('do nothing', 'RW', 'done', 2)
+print(card2)  # Card(summary='do nothing', owner='RW', state='done', id=2)
+print(card2.to_dict())  # {'summary': 'do nothing', 'owner': 'RW', 'state': 'done', 'id': 2}
 
-print(card1.to_dict())
+
+# _____________________________________
+
+def test_obj_param(obj):
+    return obj.summary
+
+
+print(test_obj_param(card2))  # do nothing
 
 # ______________________________________
-my_dict = {}
+another_dict = {}
 
 
 def create_dict(string):
-    global my_dict
+    global another_dict
     for index, item in enumerate(string):
-        my_dict[index] = item
-    return my_dict
+        another_dict[index] = item
+    return another_dict
 
 
-print(create_dict('RobPaulWelsh'))
-print(my_dict[7])
-
-
-
+print(create_dict('RobPaulWelsh'))  # {0: 'R', 1: 'o', 2: 'b', 3: 'P', 4: 'a', 5: 'u', 6: 'l', 7: 'W', 8: 'e', 9: 'l', 10: 's', 11: 'h'}
+print(another_dict[7])  # W
