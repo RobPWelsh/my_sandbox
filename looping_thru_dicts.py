e7ae9e69959bb95a5e60aeffc5b2160a962ea445
Rob Welsh <robpwelsh@gmail.com>
2024-02-14 06:57:30 -0500

    Update 2/14/24

--- looping_thru_dicts.py
+++ looping_thru_dicts.py
@@ -1,3 +1,5 @@
+from collections import UserDict
+
 guests = {'Tim': 22, 'Tonya': 45, 'Mary': 12, 'Ann': 32, 'Beth': 20, 'Sam': 5, 'Manny': 76, 'Kenton': 15, 'Kenny': 27, 'Dixie': 46, 'Jane': 35, 'Mallory': 32, 'Julian': 4, 'Edward': 71, 'Rose': 65}
 
 for x in guests:
